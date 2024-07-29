1bd588f3128d7caa3b429f8b32c8dd3596161c3d
gzj <gzj00@outlook.com>
2024-07-29 13:34:59 +0800

    chore(scripts): modify pre-commit message

--- scripts/pre-commit.ps1
+++ scripts/pre-commit.ps1
@@ -1,5 +1,5 @@
 $csFiles = Get-ChildItem -Path . -Filter *.cs
 foreach ($f in $csFiles) {
-    Write-Host "Compiling $($f.Name)..."
+    Write-Host "Formatting $($f.Name)..."
     dotnet csharpier $f.Name
 }
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,6 +2,6 @@
 
 csFiles=$(find . -maxdepth 1 -type f -name '*.cs')
 for f in $csFiles; do
-    echo "Compiling $(basename $f)..."
+    echo "Formatting $(basename $f)..."
     dotnet csharpier "$f"
 done
